aaa998b523f1413f997423588cc21b1baa9b6ab1
lazorfuzz <leontosy@gmail.com>
2019-07-08 12:00:55 -0700

    Add UI

---
+++ ui/build/precache-manifest.7cc535960f3a3bcd8fb9ec8e595e06f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea779821ed42f6e42efd24cdc2ba32d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1de93be999f5d475c42",
+    "url": "/static/css/main.3babd0af.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "a1de93be999f5d475c42",
+    "url": "/static/js/main.28647029.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
